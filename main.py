52ab07456d38db69dfc6b8d70918de090322f934
MelikaZ <uchiha.7990@gmail.com>
2023-12-27 21:21:13 +0330

    feature selection added

--- main.py
+++ main.py
@@ -1,35 +1,64 @@
 import numpy as np
 from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.operators.crossover.pntx import SinglePointCrossover
+from pymoo.operators.mutation.bitflip import BitflipMutation
+from pymoo.operators.sampling.rnd import BinaryRandomSampling
 from pymoo.optimize import minimize
 from pymoo.visualization.scatter import Scatter
 from problem import OptimisationProblem
-from sampling import CustomSamplingMax
+from sampling import CustomSampling
 from sklearn import datasets
 from mutation import CustomMutation
+import enum
+
+pop_size = 50
 
 
 def main():
-    dataset = datasets.load_iris()
+    # dataset = datasets.load_iris()
     # dataset = datasets.load_digits()
     # dataset = datasets.load_wine()
-    # dataset = datasets.load_breast_cancer()
+    dataset = datasets.load_breast_cancer()
 
-    pop_size = 50
-
-    algorithm = NSGA2(pop_size=pop_size, sampling=CustomSamplingMax())
-    problem = OptimisationProblem(dataset=dataset)
-    res = minimize(problem, algorithm, ('n_gen', 10), seed=1, verbose=False)
+    res = get_result(dataset, Method.selection)
     print(res.F)
 
-    algorithm2 = NSGA2(pop_size=pop_size, sampling=CustomSamplingMax(), mutation=CustomMutation())
-    problem = OptimisationProblem(dataset=dataset)
-    res2 = minimize(problem, algorithm2, ('n_gen', 10), seed=1, verbose=False)
+    res2 = get_result(dataset, Method.enhanced_mutation)
     print(res2.F)
 
     plot = Scatter()
     plot.add(res.F, color="red")
     plot.add(res2.F, color="blue")
+
     plot.show()
+
+
+def get_result(dataset, method):
+    problem = OptimisationProblem(dataset=dataset)
+
+    if method == Method.normal:
+        algorithm = NSGA2(pop_size=pop_size,
+                          sampling=CustomSampling())
+
+    elif method == Method.enhanced_mutation:
+        algorithm = NSGA2(pop_size=pop_size,
+                          sampling=CustomSampling(),
+                          mutation=CustomMutation())
+
+    elif method == Method.selection:
+        algorithm = NSGA2(pop_size=pop_size,
+                          sampling=BinaryRandomSampling(),
+                          crossover=SinglePointCrossover(),
+                          mutation=BitflipMutation())
+
+    res = minimize(problem, algorithm, ('n_gen', 10), seed=1, verbose=False)
+    return res
+
+
+class Method(enum.Enum):
+    normal = 1
+    enhanced_mutation = 2
+    selection = 3
 
 
 if __name__ == "__main__":
--- sampling.py
+++ sampling.py
@@ -7,20 +7,10 @@ pop_size = 50
 
 class CustomSampling(Sampling):
     def _do(self, problem, n_samples, **kwargs):
-        rand_data = np.random.random((n_samples, problem.n_var))
-        data = rand_data.reshape(-1, 1)
-        for i in range(len(data) // 20):
-            data[random.randint(0, len(data) - 1)] = 0
-        rand_data = data.reshape(pop_size, -1)
-        return rand_data
-
-
-class CustomSamplingMax(Sampling):
-    def _do(self, problem, n_samples, **kwargs):
         n_features = problem.n_var
         rand_data = np.random.random((n_samples, n_features))
         for i in range(n_samples):
-            zero_count = random.randint(0, n_features // 2)
+            zero_count = random.randint(0, n_features)
             indices = np.random.choice(np.arange(n_features), replace=False, size=zero_count)
             for index in indices:
                 rand_data[i][index] = 0
