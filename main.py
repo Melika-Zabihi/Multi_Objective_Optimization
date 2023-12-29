0dafea510cb1f02303eee7dedf3b692cf05cebd3
MelikaZ <uchiha.7990@gmail.com>
2023-12-30 01:11:26 +0330

    uci datasets added

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import numpy as np
 from pymoo.algorithms.moo.nsga2 import NSGA2
 from pymoo.operators.crossover.pntx import SinglePointCrossover
 from pymoo.operators.mutation.bitflip import BitflipMutation
@@ -10,11 +9,42 @@ from sampling import CustomSampling
 from sklearn import datasets
 from mutation import CustomMutation
 import enum
+import pandas as pd
 
-pop_size = 50
+pop_size = 100 #
+n_gen = 50
 
 
 def main():
+
+    res, res2 = uci_datasets()
+    print(res.F)
+    print(res2.F)
+
+    plot = Scatter()
+    plot.add(res.F, color="red")
+    plot.add(res2.F, color="blue")
+    plot.show()
+
+
+class Method(enum.Enum):
+    normal = 1
+    enhanced_mutation = 2
+    selection = 3
+
+
+def uci_datasets():
+    data = pd.read_csv("dataset\\arrhythmia\\arrhythmia.data", na_values="?")
+    data = data.fillna(value=0)
+
+    X = data.iloc[:, :-1].to_numpy()
+    y = data.iloc[:, -1].to_numpy()
+
+    res, res2 = get_result(X=X, y=y)
+    return res, res2
+
+
+def sklearn_datasets():
     # dataset = datasets.load_iris()
     # dataset = datasets.load_digits()
     # dataset = datasets.load_wine()
@@ -26,21 +56,17 @@ def main():
     res2 = get_result(dataset, Method.enhanced_mutation)
     print(res2.F)
 
-    plot = Scatter()
-    plot.add(res.F, color="red")
-    plot.add(res2.F, color="blue")
-
-    plot.show()
+    return res, res2
 
 
-def get_result(dataset, method):
-    problem = OptimisationProblem(dataset=dataset)
+def get_result(dataset=None, X=None, y=None, method=Method.normal):
 
-    if method == Method.normal:
-        algorithm = NSGA2(pop_size=pop_size,
-                          sampling=CustomSampling())
+    if dataset is not None:
+        problem = OptimisationProblem(dataset=dataset)
+    else:
+        problem = OptimisationProblem(X=X, y=y)
 
-    elif method == Method.enhanced_mutation:
+    if method == Method.enhanced_mutation:
         algorithm = NSGA2(pop_size=pop_size,
                           sampling=CustomSampling(),
                           mutation=CustomMutation())
@@ -51,14 +77,13 @@ def get_result(dataset, method):
                           crossover=SinglePointCrossover(),
                           mutation=BitflipMutation())
 
+    elif method == Method.normal:
+        algorithm = NSGA2(pop_size=pop_size,
+                          sampling=CustomSampling())
+
     res = minimize(problem, algorithm, ('n_gen', 10), seed=1, verbose=False)
+
     return res
-
-
-class Method(enum.Enum):
-    normal = 1
-    enhanced_mutation = 2
-    selection = 3
 
 
 if __name__ == "__main__":
