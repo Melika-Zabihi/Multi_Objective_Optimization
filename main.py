fixed optimisation objectives and normalization function

main.py
@@ -3,49 +3,52 @@ from sklearn import datasets
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from pymoo.indicators.hv import HV
 
 
 def main():
-    run_count = 5
+    run_count = 3
     results = []
     for i in range(run_count):
-        random_state = i
+        print("random state:" + str(i * 10))
+        random_state = i * 10
         res, res2 = get_sklearn_dataset_res(random_state)
         res_u = np.unique(res.F, axis=0)
         res2_u = np.unique(res2.F, axis=0)
         print(res_u)
         print(res2_u)
-        res_abs = np.abs(res_u)
-        res2_abs = np.abs(res2_u)
+
+        hv1, hv2 = calculate_hv(res, res2)
+        print("HV Selection", hv1)
+        print("HV Weighting", hv2)
 
         fig, ax = plt.subplots()
-
-        scatter1 = ax.scatter(res_abs[:, 0], res_abs[:, 1], color="red", label='Selection')
-        scatter2 = ax.scatter(res2_abs[:, 0], res2_abs[:, 1], color="blue", label='Weighting')
-
-        ax.invert_xaxis()
-        ax.invert_yaxis()
-
-        plt.xlabel('Accuracy')
-        plt.ylabel('Removed Features')
+        scatter1 = ax.scatter(res_u[:, 0], res_u[:, 1], color="red", label='Selection', alpha=0.6)
+        scatter2 = ax.scatter(res2_u[:, 0], res2_u[:, 1], color="blue", label='Weighting', alpha=0.6)
+        plt.xlabel('100 - Accuracy')
+        plt.ylabel('Remaining Features')
         plt.legend(handles=[scatter1, scatter2])
         plt.show()
 
-        hv1, hv2 = calculate_hv(res_abs,res2_abs)
+        results.append(Result(res_u, res2_u, hv1=hv1, hv2=hv2))
 
-        print("HV Selection", hv1)
-        print("HV Weighting", hv2)
-        results.append(Result(res_abs, res2_abs, hv1=hv1, hv2=hv2))
+    extract_output(results)
 
-    for i in results:
-        print("selection:")
-        print(i.get_max_acc_res1())
-        print(i.hv1)
-        print("weighting")
-        print(i.get_max_acc_res2())
-        print(i.hv2)
-        print("+++++++++++")
+
+def get_sklearn_dataset_res(random_state):
+    dataset = datasets.load_iris()
+    # dataset = datasets.load_digits()
+    # dataset = datasets.load_wine()
+    # dataset = datasets.load_breast_cancer()
+
+    res = get_result(dataset=dataset,
+                     method=Method.normal,
+                     random_state=random_state)
+
+    res2 = get_result(dataset=dataset,
+                      method=Method.enhanced_mutation,
+                      random_state=random_state)
+
+    return res, res2
 
 
 def get_uci_dataset_res(random_state):
@@ -67,31 +70,58 @@ def get_uci_dataset_res(random_state):
     return res, res2
 
 
-def calculate_hv(res, res2):
-    res_norm = 1-normalize_data(res)
-    res2_norm = 1-normalize_data(res2)
-    ref_point = np.array([1.1, 1.1])
+def extract_output(results):
+    hv1_sum = 0
+    hv2_sum = 0
+    max_acc1_sum = 0
+    acc_features1_sum = 0
+    max_acc2_sum = 0
+    acc_features2_sum = 0
+    for res in results:
+        acc1, features1 = res.get_min_rev_acc_res1()
+        max_acc1_sum += acc1
+        acc_features1_sum += features1
+        hv1_sum += res.hv1
+        acc2, features2 = res.get_min_rev_acc_res2()
+        max_acc2_sum += acc2
+        acc_features2_sum += features2
+        hv2_sum += res.hv2
 
-    ind = HV(ref_point=ref_point)
-    hv1 = ind(res_norm)
-    hv2 = ind(res2_norm)
-    return hv1, hv2
+    hv1_avg = hv1_sum / len(results)
+    hv2_avg = hv2_sum / len(results)
+    max_acc1_avg = max_acc1_sum / len(results)
+    max_acc2_avg = max_acc2_sum / len(results)
+    features1_avg = acc_features1_sum / len(results)
+    features2_avg = acc_features2_sum / len(results)
 
-def get_sklearn_dataset_res(random_state):
-    # dataset = datasets.load_iris()
-    # dataset = datasets.load_digits()
-    dataset = datasets.load_wine()
-    # dataset = datasets.load_breast_cancer()
+    print(f"hv1_avg = {hv1_avg:.2f}")
+    print(f"hv2_avg = {hv2_avg:.2f}")
+    print(f"max_acc1_avg = {max_acc1_avg:.2f}")
+    print(f"max_acc2_avg = {max_acc2_avg:.2f}")
+    print(f"features1_avg = {features1_avg:.2f}")
+    print(f"features2_avg = {features2_avg:.2f}")
 
-    res = get_result(dataset=dataset,
-                     method=Method.selection,
-                     random_state=random_state)
+    sorted_list1 = sorted(results, key=lambda x: x.hv1)
+    sorted_list2 = sorted(results, key=lambda x: x.hv2)
 
-    res2 = get_result(dataset=dataset,
-                      method=Method.enhanced_mutation,
-                      random_state=random_state)
+    middle_index1 = len(sorted_list1) // 2
+    middle_index2 = len(sorted_list2) // 2
 
-    return res, res2
+    print(sorted_list1[middle_index1].hv1)
+    print(sorted_list1[middle_index1].res1)
+    print(sorted_list2[middle_index2].hv2)
+    print(sorted_list2[middle_index2].res2)
+    res1 = sorted_list1[middle_index1].res1
+    res2 = sorted_list2[middle_index2].res2
+    fig, ax = plt.subplots()
+
+    scatter1 = ax.scatter(res1[:, 0], res1[:, 1], color="red", label='Selection', alpha=0.6)
+    scatter2 = ax.scatter(res2[:, 0], res2[:, 1], color="blue", label='Weighting', alpha=0.6)
+
+    plt.xlabel('100 - Accuracy')
+    plt.ylabel('Remaining Features')
+    plt.legend(handles=[scatter1, scatter2])
+    plt.show()
 
 
 if __name__ == "__main__":

problem.py
@@ -25,7 +25,7 @@ class OptimisationProblem(Problem):
     def _evaluate(self, dataset_weights_list, out, *args, **kwargs):
 
         accuracies = []
-        zero_weights_count = []
+        weights_count = []
         for weights in dataset_weights_list:
             weighted_X = self.X * weights
             X_train, X_test, y_train, y_test = train_test_split(weighted_X,
@@ -37,15 +37,16 @@ class OptimisationProblem(Problem):
 
             y_pred = knn.predict(X_test)
 
-            accuracy = accuracy_score(y_test, y_pred) * 100
+            reverse_accuracy = (1-accuracy_score(y_test, y_pred)) * 100
             zero_weights = np.sum(weights == 0)
+            remaining_weights = len(weights) - zero_weights
 
-            accuracies.append(-accuracy)
-            zero_weights_count.append(-zero_weights)
+            accuracies.append(reverse_accuracy)
+            weights_count.append(remaining_weights)
 
         print(self.counter)
         self.counter += 1
         print(accuracies)
-        print(zero_weights_count)
+        print(weights_count)
 
-        out["F"] = np.column_stack([accuracies, zero_weights_count])
+        out["F"] = np.column_stack([accuracies, weights_count])

utility.py
@@ -6,10 +6,11 @@ from pymoo.optimize import minimize
 from problem import OptimisationProblem
 from sampling import CustomSampling
 from mutation import CustomMutation
+from pymoo.indicators.hv import HV
 import numpy as np
 import enum
 
-pop_size = 50
+pop_size = 30
 n_gen = 10
 
 
@@ -24,7 +25,6 @@ def get_result(dataset=None, X=None, y=None, method=Method.normal, random_state=
         problem = OptimisationProblem(dataset=dataset, random_state=random_state)
     else:
         problem = OptimisationProblem(X=X, y=y, random_state=random_state)
-
     if method == Method.enhanced_mutation:
         algorithm = NSGA2(pop_size=pop_size,
                           sampling=CustomSampling(),
@@ -40,18 +40,25 @@ def get_result(dataset=None, X=None, y=None, method=Method.normal, random_state=
         algorithm = NSGA2(pop_size=pop_size,
                           sampling=CustomSampling())
 
-    res = minimize(problem, algorithm, ('n_gen', 10), seed=1, verbose=False)
-
+    res = minimize(problem, algorithm, ('n_gen', n_gen), seed=1, verbose=False)
     return res
 
 
-def normalize_data(data):
-    # Normalize the first and second elements of data separately
+def calculate_hv(res, res2):
+    res_norm = normalize_data(res)
+    res2_norm = normalize_data(res2)
+    ref_point = np.array([1.1, 1.1])
+    ind = HV(ref_point=ref_point)
+    hv1 = ind(res_norm)
+    hv2 = ind(res2_norm)
+    return hv1, hv2
+
+
+def normalize_data(res):
+    data = res.F
     data_norm = np.empty_like(data)
-    for i in range(data.shape[1]):
-        min_val = np.min(data[:, i])
-        max_val = np.max(data[:, i])
-        data_norm[:, i] = (data[:, i] - min_val) / (max_val - min_val)
+    data_norm[:, 0] = data[:, 0] / 100
+    data_norm[:, 1] = data[:, 1] / res.problem.n_var
     return data_norm
 
 
@@ -62,16 +69,20 @@ class Result:
         self.hv1 = hv1
         self.hv2 = hv2
 
-    def get_max_acc_res1(self):
-        max = 0
+    def get_min_rev_acc_res1(self):
+        min_acc = 100
+        features = 0
         for res in self.res1:
-            if res[0] > max:
-                max = res[0]
-        return max
+            if res[0] <= min_acc:
+                min_acc = res[0]
+                features = res[1]
+        return min_acc, features
 
-    def get_max_acc_res2(self):
-        max = 0
+    def get_min_rev_acc_res2(self):
+        min_acc = 100
+        features = 0
         for res in self.res2:
-            if res[0] > max:
-                max = res[0]
-        return max
+            if res[0] <= min_acc:
+                min_acc = res[0]
+                features = res[1]
+        return min_acc, features