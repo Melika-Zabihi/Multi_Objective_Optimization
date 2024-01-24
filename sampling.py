1e24ce20c2611ca418bd23b8421f3deb119c9726
MelikaZ <uchiha.7990@gmail.com>
2024-01-24 20:31:34 +0330

    custom selection sampling added

--- sampling.py
+++ sampling.py
@@ -13,3 +13,15 @@ class CustomSampling(Sampling):
             for index in indices:
                 rand_data[i][index] = 0
         return rand_data
+
+
+class CustomSelectionSampling(Sampling):
+    def _do(self, problem, n_samples, **kwargs):
+        n_features = problem.n_var
+        data = np.ones((n_samples, n_features))
+        for i in range(n_samples):
+            zero_count = random.randint(0, n_features)
+            indices = np.random.choice(np.arange(n_features), replace=False, size=zero_count)
+            for index in indices:
+                data[i][index] = 0
+        return data.astype(bool)
