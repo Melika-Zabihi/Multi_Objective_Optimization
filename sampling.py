fb7494e8f03574e5f18f10d8ab40ae769d67a1ca
MelikaZ <uchiha.7990@gmail.com>
2024-02-03 17:16:45 +0330

    Changed Weighting CustomSampling

--- sampling.py
+++ sampling.py
@@ -6,12 +6,12 @@ from pymoo.core.sampling import Sampling
 class CustomSampling(Sampling):
     def _do(self, problem, n_samples, **kwargs):
         n_features = problem.n_var
-        rand_data = np.random.random((n_samples, n_features))
+        rand_data = np.random.uniform(low=0.5, high=1, size=(n_samples, n_features))
         for i in range(n_samples):
-            zero_count = random.randint(0, n_features)
-            indices = np.random.choice(np.arange(n_features), replace=False, size=zero_count)
-            for index in indices:
-                rand_data[i][index] = 0
+            count = random.randint(0, n_features)
+            indices = np.random.choice(np.arange(n_features), replace=False, size=count)
+            new_values = np.random.uniform(low=0.0, high=0.5, size=count)
+            np.put(rand_data[i], indices, new_values)
         return rand_data
 
 
